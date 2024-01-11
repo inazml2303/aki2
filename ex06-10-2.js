e9ef4bba69eabd7d4bc660e9697ae5ff7f8ff922
inazml2303 <133722862+inazml2303@users.noreply.github.com>
2024-01-11 14:57:57 +0900

    Update ex06-10-2.js

--- ex06-10-2.js
+++ ex06-10-2.js
@@ -1,5 +1,5 @@
 function imgchange3() {
-    document.getElementById('josai').src = './image/josai2.svg';
+    document.getElementById('josai').src = './image/josai2.png';
 }
 function imgchange4() {
     document.getElementById('josai').src = './image/josai.jpg';
