91c99979ef90c91ba50c15027ff0ba779e1eee6a
inazml2303 <133722862+inazml2303@users.noreply.github.com>
2024-01-11 14:13:43 +0900

    Create ex06-10-2.js

---
+++ ex06-10-2.js
@@ -0,0 +1,6 @@
+function imgchange3() {
+    document.getElementById('josai').src = './image/josai3.svg';
+}
+function imgchange4() {
+    document.getElementById('josai').src = './image/josai2.jpg';
+}
