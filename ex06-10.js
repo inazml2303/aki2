b52244719a9dd1d83346445812ab88564a87c8bb
inazml2303 <133722862+inazml2303@users.noreply.github.com>
2024-01-11 15:32:25 +0900

    Update ex06-10.js

--- ex06-10.js
+++ ex06-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementByld('neko').src='./image/neko.jpg';}
-function imgchange2(){document.getElementByld('neko').src='./image/kuruma.jpg';}
+function imgchange1(){document.getElementByld('neko').src='./image/kuruma.jpg';}
+function imgchange2(){document.getElementByld('neko').src='./image/neko.jpg';}
